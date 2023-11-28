1a6193641db7b98e7167058f3946da6deb1ba7b7
cuhte3 <farid@getportal.co>
2023-11-28 20:24:13 +0700

    Update Party.swift

--- Sources/PortalSwapSDK/Models/Party.swift
+++ Sources/PortalSwapSDK/Models/Party.swift
@@ -1,4 +1,4 @@
-public struct Party: Codable {
+public class Party: Codable {
     struct SwapId: Codable {
         let id: String
     }
@@ -31,34 +31,38 @@ public struct Party: Codable {
         }
     }
     
-    let swap: Swap?
     let id: String
     let asset: String
     let blockchain: String
     let quantity: Int64
     let oid: String
-    let invoice: String?
+    
+    public var swap: Swap?
+    public var invoice: [String: String]?
+    var receip: [String: String]?
     
     private enum CodingKeys: String, CodingKey {
         case id, asset, blockchain, invoice, oid, quantity
     }
     
-    public var isSecretSeeker: Bool {
-        false
-    }
+    public var isSecretSeeker: Bool = false
     
-    public var isSecretHolder: Bool {
-        false
-    }
+    public var isSecretHolder: Bool = false
     
-    public init(from decoder: Decoder) throws {
+    required public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try container.decode(String.self, forKey: .id)
         asset = try container.decode(String.self, forKey: .asset)
         blockchain = try container.decode(String.self, forKey: .blockchain)
         quantity = try container.decode(Int64.self, forKey: .quantity)
         oid = try container.decode(String.self, forKey: .oid)
-        invoice = try? container.decode(String.self, forKey: .oid)
+        invoice = try? container.decode([String:String].self, forKey: .invoice)
         swap = nil
+    }
+}
+
+extension Party: Equatable {
+    public static func == (lhs: Party, rhs: Party) -> Bool {
+        lhs.id == rhs.id
     }
 }
