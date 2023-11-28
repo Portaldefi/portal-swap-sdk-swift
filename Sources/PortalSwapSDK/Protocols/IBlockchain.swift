0acf34aae49d2c3b7edbe30e88004534110e8aae
cuhte3 <farid@getportal.co>
2023-11-28 20:23:15 +0700

    Update IBlockchain.swift

--- Sources/PortalSwapSDK/Protocols/IBlockchain.swift
+++ Sources/PortalSwapSDK/Protocols/IBlockchain.swift
@@ -1,9 +1,10 @@
+import Foundation
 import Promises
 
 public protocol IBlockchain: BaseClass {
     func connect() -> Promise<Void>
     func disconnect() -> Promise<Void>
     func createInvoice(party: Party) -> Promise<[String: String]>
-    func payInvoice(party: Party) -> Promise<Void>
-    func settleInvoice(party: Party, secret: String) -> Promise<[String: String]>
+    func payInvoice(party: Party) -> Promise<[String: Any]>
+    func settleInvoice(party: Party, secret: Data) -> Promise<[String: String]>
 }
