30a7a5d2b987ab6e1803a65161c071036dbb500d
cuhte3 <farid@getportal.co>
2023-11-28 20:14:54 +0700

    Create HodlInvoice.swift

---
+++ Sources/PortalSwapSDK/Models/HodlInvoice.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+public class HodlInvoice {
+    public enum Status {
+        case awaitsPayment, paymentHeld, paymentConfirmed, paymentCanceled
+    }
+    
+    public let id: String
+    public let description: String
+    public let tokens: UInt64
+    public let paymentRequest: String
+    
+    public var subscription: InvoiceSubscription
+    
+    private var status: Status = .awaitsPayment
+    
+    public init(id: String, description: String, tokens: UInt64, paymentRequest: String) {
+        self.id = id
+        self.description = description
+        self.tokens = tokens
+        self.paymentRequest = paymentRequest
+        self.subscription = InvoiceSubscription()
+    }
+    
+    public func update(status: Status) {
+        subscription.onInvoiceUpdated?(status)
+    }
+}
