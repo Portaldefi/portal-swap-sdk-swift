620b35901b1a2c059eca8d35aeb2d33093bd1b6d
cuhte3 <farid@getportal.co>
2023-12-02 01:26:52 +0700

    Update ILightningClient.swift

--- Sources/PortalSwapSDK/Protocols/ILightningClient.swift
+++ Sources/PortalSwapSDK/Protocols/ILightningClient.swift
@@ -2,7 +2,7 @@ import Foundation
 import Promises
 
 public protocol ILightningClient {
-    func createHodlInvoice(hash: String, memo: String, quantity: Int64) -> Promise<HodlInvoice>
+    func createHodlInvoice(hash: String, memo: String, quantity: Int64) -> Promise<String>
     func subscribeToInvoice(id: String) -> Promise<InvoiceSubscription>
     func payViaPaymentRequest(swapId: String, request: String) -> Promise<PaymentResult>
     func settleHodlInvoice(secret: Data) -> Promise<[String:String]>
