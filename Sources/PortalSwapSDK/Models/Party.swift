572523d6c3f5c0cac47777bf2c9bf4518d14f350
cuhte3 <farid@getportal.co>
2023-12-08 19:00:05 +0700

    removed unused code

--- Sources/PortalSwapSDK/Models/Party.swift
+++ Sources/PortalSwapSDK/Models/Party.swift
@@ -1,36 +1,4 @@
 public class Party: Codable {
-    struct SwapId: Codable {
-        let id: String
-    }
-
-    struct Asset: Codable {
-        let name: String
-        let symbol: String
-        let contractAddress: String?
-    }
-
-    struct AssetNetwork: Codable {
-        let name: String
-        let type: String
-        
-        private enum CodingKeys: String, CodingKey {
-            case name
-            case type = "@type"
-        }
-    }
-
-    struct InvoiceCodable: Codable {
-        let created_at: String
-        let id: String
-        let mtokens: String
-        let request: String
-        let tokens: Int64
-        
-        private enum CodingKeys: String, CodingKey {
-            case created_at, id, mtokens, request, tokens
-        }
-    }
-    
     let id: String
     let asset: String
     let blockchain: String
@@ -46,7 +14,6 @@ public class Party: Codable {
     }
     
     public var isSecretSeeker: Bool = false
-    
     public var isSecretHolder: Bool = false
     
     required public init(from decoder: Decoder) throws {
