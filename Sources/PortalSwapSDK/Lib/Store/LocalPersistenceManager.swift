92052bcfb1c20ee7f629591fd1ba6eed4c6c1cd3
cuhte3 <farid@getportal.co>
2024-01-04 20:52:49 +0700

    Update LocalPersistenceManager.swift

--- Sources/PortalSwapSDK/Lib/Store/LocalPersistenceManager.swift
+++ Sources/PortalSwapSDK/Lib/Store/LocalPersistenceManager.swift
@@ -6,27 +6,14 @@ class LocalPersistenceManager: IPersistenceManager {
     var viewContext: NSManagedObjectContext { container.viewContext }
     var container: PersistentContainer
     
-    required init(
-        configuration: PersistenceConfiguration
-    ) throws {
-    
+    required init(configuration: PersistenceConfiguration) throws {
         let model = try LocalPersistenceManager.model(for: configuration.modelName)
-
-        self.container = .init(name: configuration.modelName, managedObjectModel: model)
         
-        if let description = self.container.persistentStoreDescriptions.first {
-            description.configuration = configuration.configuration
-            description.type = NSInMemoryStoreType
-            description.shouldMigrateStoreAutomatically = true
-            description.shouldInferMappingModelAutomatically = true
-        } else {
-            // Handle the case where there is no persistent store description
-            fatalError("No persistent store descriptions found in the container.")
-        }
-        
-        self.container.loadPersistentStores(completionHandler: { (desc, err) in
+        container = .init(name: configuration.modelName, managedObjectModel: model)
+        container.loadPersistentStores(completionHandler: { (desc, err) in
             if let err = err {
-                fatalError("Error loading LOCAL STORE: \(desc): \(err)")
+                debugPrint("Error loading LOCAL STORE: \(desc): \(err)")
+                return
             }
             debugPrint("Loaded TEMPORARY STORE successfully")
         })
@@ -35,8 +22,14 @@ class LocalPersistenceManager: IPersistenceManager {
 
 extension IPersistenceManager {
     static func model(for name: String) throws -> NSManagedObjectModel {
-        guard let url = Bundle.module.url(forResource: name, withExtension: "momd") else { throw SwapSDKError.msg("Could not get URL for model: \(name)") }
-        guard let model = NSManagedObjectModel(contentsOf: url) else { throw SwapSDKError.msg("Could not get model for: \(url)") }
+        guard let url = Bundle.module.url(forResource: name, withExtension: "momd") else {
+            throw SwapSDKError.msg("Could not get URL for model: \(name)")
+        }
+        
+        guard let model = NSManagedObjectModel(contentsOf: url) else {
+            throw SwapSDKError.msg("Could not get model for: \(url)")
+        }
+        
         return model
     }
 }
