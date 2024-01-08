ae2cecf7f73276f34de01c471616563bdf23737a
cuhte3 <farid@getportal.co>
2024-01-08 20:30:45 +0700

    Update Store.swift
    utilise helper functions

--- Sources/PortalSwapSDK/Lib/Store/Store.swift
+++ Sources/PortalSwapSDK/Lib/Store/Store.swift
@@ -25,6 +25,7 @@ class Store: BaseClass {
                 )
                 
                 emit(event: "open", args: [])
+                
                 resolve(())
             } catch {
                reject(error)
@@ -42,43 +43,13 @@ class Store: BaseClass {
             throw SwapSDKError.msg("Cannot obtain persistenceManager")
         }
 
+        let viewContext = persistenceManager.viewContext
+        
         switch namespace {
         case .secrets:
-            let request = DBSecret.fetchRequest() as NSFetchRequest<DBSecret>
-            let secrets = try persistenceManager.viewContext.fetch(request)
-            
-            if let secret = secrets.first(where: { $0.secretHash == key }) {
-                return ["secret" : secret.data as Any]
-            } else {
-                throw SwapSDKError.msg("secret with id: \(key) is not exists in DB")
-            }
+            return try DBSecret.entity(key: key, context: viewContext).toJSON()
         case .swaps:
-            let request = DBSwap.fetchRequest() as NSFetchRequest<DBSwap>
-            let swaps = try persistenceManager.viewContext.fetch(request)
-            
-            if let swap = swaps.first(where: { $0.swapID == key }) {
-                return [
-                    "id": swap.swapID as Any,
-                    "status": swap.status as Any,
-                    "secretHash": swap.secretHash as Any,
-                    "secretSeeker" : [
-                        "asset" : swap.secretSeeker?.asset as Any,
-                        "id": swap.secretSeeker?.partyID as Any,
-                        "quantity" : swap.secretSeeker?.quantity as Any,
-                        "oid" : swap.secretSeeker?.oid as Any,
-                        "blockchain" : swap.secretSeeker?.blockchain as Any
-                    ],
-                    "secretHolder" : [
-                        "asset" : swap.secretHolder?.asset as Any,
-                        "id": swap.secretHolder?.partyID as Any,
-                        "quantity" : swap.secretHolder?.quantity as Any,
-                        "oid" : swap.secretHolder?.oid as Any,
-                        "blockchain" : swap.secretHolder?.blockchain as Any
-                    ]
-                ]
-            } else {
-                throw SwapSDKError.msg("Swap with id: \(key) is not exists in DB")
-            }
+            return try DBSwap.entity(key: key, context: viewContext).toJSON()
         }
     }
     
@@ -86,104 +57,24 @@ class Store: BaseClass {
         guard let persistenceManager = persistenceManager else {
             throw SwapSDKError.msg("Cannot obtain persistenceManager")
         }
+        
+        let viewContext = persistenceManager.viewContext
 
         switch namespace {
         case .secrets:
-            if
-                let dataDict = obj as? [String:String],
-                let swapId = dataDict["swap"],
-                let secretString = dataDict["secret"],
-                let secret = Utils.hexToData(secretString)
-            {
-                let newEntity = DBSecret(context: persistenceManager.viewContext)
-                
-                newEntity.data = secret
-                newEntity.swapID = swapId
-                newEntity.secretHash = key
-            } else {
-                throw SwapSDKError.msg("Cannot unwrap secret data")
-            }
+            let newEntity = DBSecret(context: viewContext)
+            try newEntity.update(json: obj, key: key)
+            
+            debug("STORE Put secret with ID: \(newEntity.swapID ?? "Unknown")")
         case .swaps:
-            let jsonData = try JSONSerialization.data(withJSONObject: obj, options: [])
-            let swap = try JSONDecoder().decode(Swap.self, from: jsonData)
+            let swap = try Swap.from(json: obj)
+            let newEntity = DBSwap(context: viewContext)
+            try newEntity.update(swap: swap)
             
-            let request = DBSwap.fetchRequest() as NSFetchRequest<DBSwap>
-            let swaps = try persistenceManager.viewContext.fetch(request)
-            
-            if let dbSwap = swaps.first(where: { $0.swapID == key }) {
-                dbSwap.swapID = swap.id
-                dbSwap.secretHash = swap.secretHash
-                dbSwap.status = swap.status
-                
-                dbSwap.secretSeeker?.partyID = swap.secretSeeker.id
-                dbSwap.secretSeeker?.oid = swap.secretSeeker.oid
-                dbSwap.secretSeeker?.blockchain = swap.secretSeeker.blockchain
-                dbSwap.secretSeeker?.asset = swap.secretSeeker.asset
-                dbSwap.secretSeeker?.quantity = swap.secretSeeker.quantity
-                
-                if let seekerInvoice = swap.secretSeeker.invoice {
-                    if seekerInvoice["request"] != nil {
-                        // Lightning Invoice
-                        dbSwap.secretSeeker?.invoice?.lightningInvoice?.invoiceID = swap.secretSeeker.invoice?["id"]
-                        dbSwap.secretSeeker?.invoice?.lightningInvoice?.request = swap.secretSeeker.invoice?["request"]
-                        dbSwap.secretSeeker?.invoice?.lightningInvoice?.swap = swap.secretSeeker.invoice?["swap"]
-                    } else {
-                        //EVM Invoice
-                        dbSwap.secretSeeker?.invoice?.evmInvoice?.blockHash = swap.secretSeeker.invoice?["blockHash"]
-                        dbSwap.secretSeeker?.invoice?.evmInvoice?.from = swap.secretSeeker.invoice?["from"]
-                        dbSwap.secretSeeker?.invoice?.evmInvoice?.to = swap.secretSeeker.invoice?["to"]
-                        dbSwap.secretSeeker?.invoice?.evmInvoice?.transactionHash = swap.secretSeeker.invoice?["transactionHash"]
-                    }
-                } else if let holderInvoice = swap.secretHolder.invoice {
-                    if holderInvoice["request"] != nil {
-                        // Lightning Invoice
-                        dbSwap.secretHolder?.invoice?.lightningInvoice?.invoiceID = swap.secretSeeker.invoice?["id"]
-                        dbSwap.secretHolder?.invoice?.lightningInvoice?.request = swap.secretSeeker.invoice?["request"]
-                        dbSwap.secretHolder?.invoice?.lightningInvoice?.swap = swap.secretSeeker.invoice?["swap"]
-                    } else {
-                        //EVM Invoice
-                        dbSwap.secretHolder?.invoice?.evmInvoice?.blockHash = swap.secretSeeker.invoice?["blockHash"]
-                        dbSwap.secretHolder?.invoice?.evmInvoice?.from = swap.secretSeeker.invoice?["from"]
-                        dbSwap.secretHolder?.invoice?.evmInvoice?.to = swap.secretSeeker.invoice?["to"]
-                        dbSwap.secretHolder?.invoice?.evmInvoice?.transactionHash = swap.secretSeeker.invoice?["transactionHash"]
-                    }
-                }
-                                    
-                dbSwap.secretHolder?.partyID = swap.secretHolder.id
-                dbSwap.secretHolder?.oid = swap.secretHolder.oid
-                dbSwap.secretHolder?.blockchain = swap.secretHolder.blockchain
-                dbSwap.secretHolder?.asset = swap.secretHolder.asset
-                dbSwap.secretHolder?.quantity = swap.secretHolder.quantity
-            } else {
-                let newEntity = DBSwap(context: persistenceManager.viewContext)
-                
-                newEntity.swapID = swap.id
-                newEntity.secretHash = swap.secretHash
-                newEntity.status = swap.status
-                
-                let secretSeeker = DBParty(context: persistenceManager.viewContext)
-                secretSeeker.partyID = swap.secretSeeker.id
-                secretSeeker.oid = swap.secretSeeker.oid
-                secretSeeker.blockchain = swap.secretSeeker.blockchain
-                secretSeeker.asset = swap.secretSeeker.asset
-                secretSeeker.quantity = swap.secretSeeker.quantity
-                secretSeeker.invoice = DBInvoice(context: persistenceManager.viewContext)
-                
-                newEntity.secretSeeker = secretSeeker
-                
-                let secretHolder = DBParty(context: persistenceManager.viewContext)
-                secretHolder.partyID = swap.secretHolder.id
-                secretHolder.oid = swap.secretHolder.oid
-                secretHolder.blockchain = swap.secretHolder.blockchain
-                secretHolder.asset = swap.secretHolder.asset
-                secretHolder.quantity = swap.secretHolder.quantity
-                secretHolder.invoice = DBInvoice(context: persistenceManager.viewContext)
-                
-                newEntity.secretHolder = secretHolder
-            }
+            debug("STORE Put swap with ID: \(newEntity.swapID ?? "Unknown")")
         }
         
-        try persistenceManager.viewContext.save()
+        try viewContext.save()
     }
     
     func update(_ namespace: StoreNamespace, _ key: String, _ obj: [String: Any]) throws {
@@ -191,68 +82,20 @@ class Store: BaseClass {
             throw SwapSDKError.msg("Cannot obtain persistenceManager")
         }
         
+        let viewContext = persistenceManager.viewContext
+        
         switch namespace {
         case .swaps:
-            let jsonData = try JSONSerialization.data(withJSONObject: obj, options: [])
-            let swap = try JSONDecoder().decode(Swap.self, from: jsonData)
+            let swap = try Swap.from(json: obj)
+            let dbSwap = try DBSwap.entity(key: key, context: viewContext)
+            try dbSwap.update(swap: swap)
             
-            let request = DBSwap.fetchRequest() as NSFetchRequest<DBSwap>
-            let swaps = try persistenceManager.viewContext.fetch(request)
-                
-            if let dbSwap = swaps.first(where: { $0.swapID == key }) {
-                dbSwap.swapID = swap.id
-                dbSwap.secretHash = swap.secretHash
-                dbSwap.status = swap.status
-                
-                dbSwap.secretSeeker?.partyID = swap.secretSeeker.id
-                dbSwap.secretSeeker?.oid = swap.secretSeeker.oid
-                dbSwap.secretSeeker?.blockchain = swap.secretSeeker.blockchain
-                dbSwap.secretSeeker?.asset = swap.secretSeeker.asset
-                dbSwap.secretSeeker?.quantity = swap.secretSeeker.quantity
-                
-                if let seekerInvoice = swap.secretSeeker.invoice {
-                    if seekerInvoice["request"] != nil {
-                        // Lightning Invoice
-                        dbSwap.secretSeeker?.invoice?.lightningInvoice?.invoiceID = swap.secretSeeker.invoice?["id"]
-                        dbSwap.secretSeeker?.invoice?.lightningInvoice?.request = swap.secretSeeker.invoice?["request"]
-                        dbSwap.secretSeeker?.invoice?.lightningInvoice?.swap = swap.secretSeeker.invoice?["swap"]
-                    } else {
-                        //EVM Invoice
-                        dbSwap.secretSeeker?.invoice?.evmInvoice?.blockHash = swap.secretSeeker.invoice?["blockHash"]
-                        dbSwap.secretSeeker?.invoice?.evmInvoice?.from = swap.secretSeeker.invoice?["from"]
-                        dbSwap.secretSeeker?.invoice?.evmInvoice?.to = swap.secretSeeker.invoice?["to"]
-                        dbSwap.secretSeeker?.invoice?.evmInvoice?.transactionHash = swap.secretSeeker.invoice?["transactionHash"]
-                    }
-                } else if let holderInvoice = swap.secretHolder.invoice {
-                    if holderInvoice["request"] != nil {
-                        // Lightning Invoice
-                        dbSwap.secretHolder?.invoice?.lightningInvoice?.invoiceID = swap.secretSeeker.invoice?["id"]
-                        dbSwap.secretHolder?.invoice?.lightningInvoice?.request = swap.secretSeeker.invoice?["request"]
-                        dbSwap.secretHolder?.invoice?.lightningInvoice?.swap = swap.secretSeeker.invoice?["swap"]
-                    } else {
-                        //EVM Invoice
-                        dbSwap.secretHolder?.invoice?.evmInvoice?.blockHash = swap.secretSeeker.invoice?["blockHash"]
-                        dbSwap.secretHolder?.invoice?.evmInvoice?.from = swap.secretSeeker.invoice?["from"]
-                        dbSwap.secretHolder?.invoice?.evmInvoice?.to = swap.secretSeeker.invoice?["to"]
-                        dbSwap.secretHolder?.invoice?.evmInvoice?.transactionHash = swap.secretSeeker.invoice?["transactionHash"]
-                    }
-                }
-                                    
-                dbSwap.secretHolder?.partyID = swap.secretHolder.id
-                dbSwap.secretHolder?.oid = swap.secretHolder.oid
-                dbSwap.secretHolder?.blockchain = swap.secretHolder.blockchain
-                dbSwap.secretHolder?.asset = swap.secretHolder.asset
-                dbSwap.secretHolder?.quantity = swap.secretHolder.quantity
-                
-                debug("Updating db swap with status: \(dbSwap.status ?? "Unknown")")
-                
-                try persistenceManager.viewContext.save()
-            } else {
-                throw SwapSDKError.msg("Swap with id: \(key) is not exists in DB")
-            }
+            debug("Updating db swap with status: \(dbSwap.status ?? "Unknown")")
         default:
             break
         }
+        
+        try viewContext.save()
     }
     
     func del(id: String) throws {
