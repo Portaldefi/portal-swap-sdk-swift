abc014a6df8129b068b138ac15f3631202ce0075
cuhte3 <farid@getportal.co>
2024-01-17 21:20:11 +0700

    Update LocalPersistenceManager.swift
    api added:
    internal func saveContext() throws
    internal func secretEntity() -> DBSecret
    internal func secret(key: String) throws -> DBSecret
    internal func swapEntity() -> DBSwap
    internal func swap(key: String) throws -> DBSwap
    public func fetchSwaps() throws -> [SwapModel]

--- Sources/PortalSwapSDK/Lib/Store/LocalPersistenceManager.swift
+++ Sources/PortalSwapSDK/Lib/Store/LocalPersistenceManager.swift
@@ -1,12 +1,11 @@
 import Foundation
 import CoreData
 
-public class LocalPersistenceManager: IPersistenceManager {
+public final class LocalPersistenceManager {
+    private var viewContext: NSManagedObjectContext { container.viewContext }
+    private var container: PersistentContainer
     
-    public var viewContext: NSManagedObjectContext { container.viewContext }
-    var container: PersistentContainer
-    
-    public required init(configuration: PersistenceConfiguration) throws {
+    fileprivate init(configuration: PersistenceConfiguration) throws {
         let model = try LocalPersistenceManager.model(for: configuration.modelName)
         
         container = .init(name: configuration.modelName, managedObjectModel: model)
@@ -18,9 +17,46 @@ public class LocalPersistenceManager: IPersistenceManager {
             print("Loaded LOCAL STORE successfully")
         })
     }
+    
+    internal func saveContext() throws {
+        try viewContext.save()
+    }
+    
+    internal func secretEntity() -> DBSecret {
+        DBSecret(context: viewContext)
+    }
+    
+    internal func secret(key: String) throws -> DBSecret {
+        try DBSecret.entity(key: key, context: viewContext)
+    }
+    
+    internal func swapEntity() -> DBSwap {
+        DBSwap(context: viewContext)
+    }
+    
+    internal func swap(key: String) throws -> DBSwap {
+        try DBSwap.entity(key: key, context: viewContext)
+    }
+    
+    public func fetchSwaps() throws -> [SwapModel] {
+        try DBSwap.entities(context: viewContext).compactMap{ try SwapModel(record: $0) }
+    }
 }
 
-extension IPersistenceManager {
+extension LocalPersistenceManager {
+    static public func manager() throws -> LocalPersistenceManager {
+        let config: PersistenceConfiguration = .init(
+            modelName: "DBModel",
+            cloudIdentifier: String(),
+            configuration: "Local"
+        )
+        return try LocalPersistenceManager(configuration: config)
+    }
+    
+    static func manager(configuration: PersistenceConfiguration) throws -> LocalPersistenceManager {
+        return try LocalPersistenceManager(configuration: configuration)
+    }
+    
     static func model(for name: String) throws -> NSManagedObjectModel {
         guard let url = Bundle.module.url(forResource: name, withExtension: "momd") else {
             throw SwapSDKError.msg("Could not get URL for model: \(name)")
