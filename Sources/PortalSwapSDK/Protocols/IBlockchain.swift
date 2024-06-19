352804fa53574e066d0a0ba0d57fd31fcf228b55
cuhte3 <farid@getportal.co>
2024-06-20 00:11:32 +0700

    Update IBlockchain.swift
    func registerSwap(intent: SwapIntent) -> Promise<[String: String]>

--- Sources/PortalSwapSDK/Protocols/IBlockchain.swift
+++ Sources/PortalSwapSDK/Protocols/IBlockchain.swift
@@ -4,6 +4,7 @@ import Promises
 protocol IBlockchain: BaseClass {
     func connect() -> Promise<Void>
     func disconnect() -> Promise<Void>
+    func registerSwap(intent: SwapIntent) -> Promise<[String: String]>
     func createInvoice(party: Party) -> Promise<[String: String]>
     func payInvoice(party: Party) -> Promise<[String: Any]>
     func settleInvoice(party: Party, secret: Data) -> Promise<[String: String]>
