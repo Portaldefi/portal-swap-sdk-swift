bd0a4b7cec97649b34ab737c1edf7d4c31b24282
cuhte3 <farid@getportal.co>
2024-06-20 00:12:34 +0700

    Create SwapIntent.swift

---
+++ Sources/PortalSwapSDK/Models/SwapIntent.swift
@@ -0,0 +1,27 @@
+//
+//  SwapIntent.swift
+//  
+//
+//  Created by farid on 14.06.2024.
+//
+
+import Foundation
+
+public struct SwapIntent {
+    public let traderBuyId: UInt64
+    public let buyAmount: UInt64
+    public let buyAddress: String
+    public let sellAmount: UInt64
+    public let sellAddress: String
+    public let buyAmountSlippage: UInt64
+    
+    public init(traderBuyId: UInt64, buyAmount: UInt64, buyAddress: String, sellAmount: UInt64, sellAddress: String, buyAmountSlippage: UInt64) {
+        self.traderBuyId = traderBuyId
+        self.buyAmount = buyAmount
+        self.buyAddress = buyAddress
+        self.sellAmount = sellAmount
+        self.sellAddress = sellAddress
+        self.buyAmountSlippage = buyAmountSlippage
+    }
+}
+
