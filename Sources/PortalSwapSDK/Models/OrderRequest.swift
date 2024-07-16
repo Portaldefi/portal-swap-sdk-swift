f42632929540d06bc0fcd86bd80ef67be165a5c3
cuhte3 <farid@getportal.co>
2024-07-16 20:08:21 +0700

    Update OrderRequest.swift

--- Sources/PortalSwapSDK/Models/OrderRequest.swift
+++ Sources/PortalSwapSDK/Models/OrderRequest.swift
@@ -1,19 +1,21 @@
+import Foundation
+
 public struct OrderRequest {
-    public let baseAsset: String
-    public let baseNetwork: String
-    public let baseQuantity: Int
-    public let quoteAsset: String
-    public let quoteNetwork: String
-    public let quoteQuantity: Int
-    public let side: String
+    public let secret: Data
+    public let buyAsset: String
+    public let buyNetwork: String
+    public let buyQuantity: Int
+    public let sellAsset: String
+    public let sellNetwork: String
+    public let sellQuantity: Int
     
-    public init(baseAsset: String, baseNetwork: String, baseQuantity: Int, quoteAsset: String, quoteNetwork: String, quoteQuantity: Int, side: String) {
-        self.baseAsset = baseAsset
-        self.baseNetwork = baseNetwork
-        self.baseQuantity = baseQuantity
-        self.quoteAsset = quoteAsset
-        self.quoteNetwork = quoteNetwork
-        self.quoteQuantity = quoteQuantity
-        self.side = side
+    public init(secret: Data, buyAsset: String, buyNetwork: String, buyQuantity: Int, sellAsset: String, sellNetwork: String, sellQuantity: Int) {
+        self.secret = secret
+        self.buyAsset = buyAsset
+        self.buyNetwork = buyNetwork
+        self.buyQuantity = buyQuantity
+        self.sellAsset = sellAsset
+        self.sellNetwork = sellNetwork
+        self.sellQuantity = sellQuantity
     }
 }
