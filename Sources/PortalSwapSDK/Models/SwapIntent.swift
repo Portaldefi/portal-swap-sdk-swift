1389ebd78d511a0b60241aca32eb002af7998f3d
cuhte3 <farid@getportal.co>
2024-07-16 20:08:29 +0700

    Update SwapIntent.swift

--- Sources/PortalSwapSDK/Models/SwapIntent.swift
+++ Sources/PortalSwapSDK/Models/SwapIntent.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 public struct SwapIntent {
+    public let secretHash: Data
     public let traderBuyId: UInt64
     public let buyAmount: UInt64
     public let buyAddress: String
@@ -15,7 +16,8 @@ public struct SwapIntent {
     public let sellAddress: String
     public let buyAmountSlippage: UInt64
     
-    public init(traderBuyId: UInt64, buyAmount: UInt64, buyAddress: String, sellAmount: UInt64, sellAddress: String, buyAmountSlippage: UInt64) {
+    public init(secretHash: Data, traderBuyId: UInt64, buyAmount: UInt64, buyAddress: String, sellAmount: UInt64, sellAddress: String, buyAmountSlippage: UInt64) {
+        self.secretHash = secretHash
         self.traderBuyId = traderBuyId
         self.buyAmount = buyAmount
         self.buyAddress = buyAddress
