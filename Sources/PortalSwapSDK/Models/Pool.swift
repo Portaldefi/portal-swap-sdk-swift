3625a984dcae3f22ce146a4009dbed9ae4a726ff
cuhte3 <farid@getportal.co>
2024-08-22 14:51:19 +0700

    Create Pool.swift

---
+++ Sources/PortalSwapSDK/Models/Pool.swift
@@ -0,0 +1,31 @@
+import Foundation
+import Web3
+
+public struct Pool: Identifiable {
+    public struct Asset: Identifiable {
+        public let id: EthereumAddress
+        public let name: String
+        public let symbol: String
+        public let logo: String
+        public let blockchainId: BigUInt
+        public let blockchainName: String
+        public let blockchainAddress: String
+        public let blockchainDecimals: UInt8
+    }
+    
+    public let id: String
+    public let baseAsset: Asset
+    public let quoteAsset: Asset
+    public let fee: BigUInt
+    public let minOrderSize: BigUInt
+    public let maxOrderSize: BigUInt
+    
+    init(model: PoolModel, baseAsset: Asset, quoteAsset: Asset) {
+        self.id = model.id.toHexString()
+        self.baseAsset = baseAsset
+        self.quoteAsset = quoteAsset
+        self.fee = model.fee
+        self.minOrderSize = model.minOrderSize
+        self.maxOrderSize = model.maxOrderSize
+    }
+}
