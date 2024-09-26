e8d8e2362c0e36ed0b0fcb31e285126bcc426d1c
cuhte3 <farid@getportal.co>
2024-09-26 15:51:41 +0700

    Update OrderCreatedEvent.swift

--- Sources/PortalSwapSDK/Models/ContractEvents/OrderCreatedEvent.swift
+++ Sources/PortalSwapSDK/Models/ContractEvents/OrderCreatedEvent.swift
@@ -55,3 +55,62 @@ struct OrderCreatedEvent: Codable {
     public func encode(to encoder: Encoder) throws {}
 }
 
+struct AuthorizedEvent: Codable {
+    // Event properties
+    let address: String
+    let topics: [String]
+    let data: String
+    let blockNumber: String
+    let transactionHash: String
+    let transactionIndex: String
+    let blockHash: String
+    let logIndex: String
+    let removed: Bool
+    
+    // Decoded parameter
+    let swapId: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case address, topics, data, blockNumber, transactionHash, transactionIndex, blockHash, logIndex, removed
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        // Decode all required fields
+        address = try container.decode(String.self, forKey: .address)
+        topics = try container.decode([String].self, forKey: .topics)
+        data = try container.decode(String.self, forKey: .data)
+        blockNumber = try container.decode(String.self, forKey: .blockNumber)
+        transactionHash = try container.decode(String.self, forKey: .transactionHash)
+        transactionIndex = try container.decode(String.self, forKey: .transactionIndex)
+        blockHash = try container.decode(String.self, forKey: .blockHash)
+        logIndex = try container.decode(String.self, forKey: .logIndex)
+        removed = try container.decode(Bool.self, forKey: .removed)
+        
+        // Decode swapId from data
+        guard let decodedSwapId = AuthorizedEvent.decodeSwapId(from: data) else {
+            throw DecodingError.dataCorruptedError(forKey: .data, in: container, debugDescription: "Unable to decode swapId from data")
+        }
+        swapId = decodedSwapId
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        // Implement encoding if necessary
+    }
+    
+    // Function to decode swapId from data
+    static func decodeSwapId(from data: String) -> String? {
+        // Remove '0x' prefix if present
+        let dataHexString = data.hasPrefix("0x") ? String(data.dropFirst(2)) : data
+        
+        // Ensure data is 64 hex characters (32 bytes)
+        guard dataHexString.count == 64 else {
+            print("Data length is not 64 hex characters")
+            return nil
+        }
+        
+        // Return the swapId with '0x' prefix
+        return "0x" + dataHexString
+    }
+}
