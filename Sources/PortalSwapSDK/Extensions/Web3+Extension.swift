e405baf6140949f1390bfa8cb5b971cc8f9513c3
cuhte3 <farid@getportal.co>
2024-10-22 16:03:10 +0700

    Update Web3+Extension.swift
    added web3 promisses api wrapper:
    - blockNumber
    - getLogs
    - fetchReceipt

--- Sources/PortalSwapSDK/Extensions/Web3+Extension.swift
+++ Sources/PortalSwapSDK/Extensions/Web3+Extension.swift
@@ -2,6 +2,51 @@ import Web3
 import Promises
 
 extension Web3.Eth {
+    func blockNumber() -> Promise<EthereumQuantity> {
+        Promise { resolve, reject in
+            blockNumber { response in
+                switch response.status {
+                case .success(let blockNumber):
+                    resolve(blockNumber)
+                case .failure(let error):
+                    reject(error)
+                }
+            }
+        }
+    }
+    
+    func getLogs(addresses: [EthereumAddress]?, topics: [[EthereumData]]?, fromBlock: EthereumQuantityTag, toBlock: EthereumQuantityTag) -> Promise<[EthereumLogObject]> {
+        Promise { resolve, reject in
+            getLogs(addresses: addresses, topics: topics, fromBlock: fromBlock, toBlock: toBlock) { response in
+                switch response.status {
+                case .success(let logs):
+                    guard !logs.isEmpty else {
+                        return reject(SwapSDKError.msg("Empty logs"))
+                    }
+                    resolve(logs)
+                case .failure(let error):
+                    reject(error)
+                }
+            }
+        }
+    }
+    
+    func fetchReceipt(txHash: EthereumData) -> Promise<EthereumTransactionReceiptObject> {
+        Promise { resolve, reject in
+            getTransactionReceipt(transactionHash: txHash) { response in
+                switch response.status {
+                case .success(let receipt):
+                    guard let receipt else {
+                        return reject(SwapSDKError.msg("Fething receipt failed"))
+                    }
+                    resolve(receipt)
+                case .failure(let error):
+                    reject(error)
+                }
+            }
+        }
+    }
+    
     func getNonce(address: EthereumAddress) -> Promise<EthereumQuantity> {
         Promise { resolve, reject in
             getTransactionCount(address: address, block: .latest) { nonceResponse in
