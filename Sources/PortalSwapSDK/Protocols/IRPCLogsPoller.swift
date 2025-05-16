59714c7e6490250ca37371e1db69d73b4e6e9769
cuhte3 <farid@getportal.co>
2025-05-16 14:11:44 +0400

    Update IRPCLogsPoller.swift

--- Sources/PortalSwapSDK/Protocols/IRPCLogsPoller.swift
+++ Sources/PortalSwapSDK/Protocols/IRPCLogsPoller.swift
@@ -1,3 +1,7 @@
+import Foundation
+import Web3
+import Web3ContractABI
+
 protocol IRPCLogsPoller {
     func watchContractEvents(interval: TimeInterval, onLogs: @escaping ([EthereumLogObject]) -> Void, onError: @escaping (Error) -> Void)
     func stopWatchingContractEvents()
