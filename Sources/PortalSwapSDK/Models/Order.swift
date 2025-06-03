f702d402b7e2061631751ae8f27aea5cae49913b
cuhte3 <farid@getportal.co>
2025-06-03 23:23:29 +0300

    Update Order.swift

--- Sources/PortalSwapSDK/Models/Order.swift
+++ Sources/PortalSwapSDK/Models/Order.swift
@@ -1,106 +1,53 @@
-public struct Order: Decodable {
+import Foundation
+import BigInt
+
+public class Order {
+    public enum OrderType: Int8 {
+        case market = 0, limit
+    }
+
     public let id: String
-    public let ts: Int64
-    public let uid: String
-    public let type: OrderType
-    public let side: OrderSide
-    public let baseAsset: String
-    public let baseQuantity: Int32
-    public let baseNetwork: String
-    public let quoteAsset: String
-    public let quoteQuantity: Int64
-    public let quoteNetwork: String
-    public let status: OrderStatus
-    public let reason: String?
+    public let ts: Int
+    public let trader: String
+    public let sellAsset: String
+    public let sellAmount: BigInt
+    public let buyAsset: String
+    public let buyAmount: BigInt
+    public let orderType: OrderType
     
-    public enum OrderType: String, Decodable {
-        case limit, market
-    }
-
-    public enum OrderSide: String, Decodable {
-        case ask, bid
-    }
-    
-    public enum OrderStatus: String, Decodable {
-        case created, opened, commiting, commited
-    }
-
-    enum CodingKeys: String, CodingKey {
-        case id, 
-             ts,
-             uid,
-             type,
-             side,
-             hash,
-             baseAsset,
-             baseQuantity,
-             baseNetwork,
-             quoteAsset,
-             quoteQuantity,
-             quoteNetwork,
-             status,
-             reason
-    }
-    
-    public init(
-        id: String,
-        ts: Int64,
-        uid: String,
-        type: OrderType,
-        side: OrderSide,
-        baseAsset: String,
-        baseQuantity: Int32,
-        baseNetwork: String,
-        quoteAsset: String,
-        quoteQuantity: Int64,
-        quoteNetwork: String,
-        status: OrderStatus,
-        reason: String?
-    ) {
-        self.id = id
-        self.ts = ts
-        self.uid = uid
-        self.type = type
-        self.side = side
-        self.baseAsset = baseAsset
-        self.baseQuantity = baseQuantity
-        self.baseNetwork = baseNetwork
-        self.quoteAsset = quoteAsset
-        self.quoteQuantity = quoteQuantity
-        self.quoteNetwork = quoteNetwork
-        self.status = status
-        self.reason = reason
-    }
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(String.self, forKey: .id)
-        ts = try container.decode(Int64.self, forKey: .ts)
-        uid = try container.decode(String.self, forKey: .uid)
-        baseAsset = try container.decode(String.self, forKey: .baseAsset)
-        baseQuantity = try container.decode(Int32.self, forKey: .baseQuantity)
-        baseNetwork = try container.decode(String.self, forKey: .baseNetwork)
-        quoteAsset = try container.decode(String.self, forKey: .quoteAsset)
-        quoteQuantity = try container.decode(Int64.self, forKey: .quoteQuantity)
-        quoteNetwork = try container.decode(String.self, forKey: .quoteNetwork)
-        reason = try container.decodeIfPresent(String.self, forKey: .reason)
+    public init(id: String? = nil, ts: Int? = nil, trader: String, sellAsset: String, sellAmount: BigInt, buyAsset: String, buyAmount: BigInt, orderType: OrderType) throws {
+        if sellAsset == buyAsset {
+            throw SdkError(message: "Sell and buy assets cannot be the same", code: "Order")
+        }
         
-        let statusString = try container.decode(String.self, forKey: .status)
-        guard let typeValue = OrderStatus(rawValue: statusString) else {
-            throw DecodingError.dataCorruptedError(forKey: .status, in: container, debugDescription: "Invalid order status")
+        if sellAmount < 0 {
+            throw SdkError(message: "Sell amount must be a positive number", code: "Order")
         }
-        status = typeValue
-
-        let typeString = try container.decode(String.self, forKey: .type)
-        guard let typeValue = OrderType(rawValue: typeString) else {
-            throw DecodingError.dataCorruptedError(forKey: .type, in: container, debugDescription: "Invalid order type")
+        
+        if buyAmount < 0 {
+            throw SdkError(message: "Buy amount must be a positive number", code: "Order")
         }
-        type = typeValue
-
-        let sideString = try container.decode(String.self, forKey: .side)
-        guard let sideValue = OrderSide(rawValue: sideString) else {
-            throw DecodingError.dataCorruptedError(forKey: .side, in: container, debugDescription: "Invalid swap side")
-        }
-        side = sideValue
+        
+        self.id = id ?? String(repeating: "0", count: 66) // "0x" + 64 zeros
+        self.ts = ts ?? 0
+        self.trader = trader
+        self.sellAsset = sellAsset
+        self.sellAmount = sellAmount
+        self.buyAsset = buyAsset
+        self.buyAmount = buyAmount
+        self.orderType = orderType
     }
+    
+    public func toJSON() -> [String: Any] {
+        return [
+            "id": id,
+            "ts": ts,
+            "trader": trader,
+            "sellAsset": sellAsset,
+            "sellAmount": sellAmount.description,
+            "buyAsset": buyAsset,
+            "buyAmount": buyAmount.description,
+            "orderType": orderType.rawValue
+        ]
+    }    
 }
