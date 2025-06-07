6cbb3fa09d00c65e72f43876cc374de05cafd184
cuhte3 <farid@getportal.co>
2025-06-07 18:09:50 +0300

    Update Pool.swift

--- Sources/PortalSwapSDK/Models/Pool.swift
+++ Sources/PortalSwapSDK/Models/Pool.swift
@@ -11,21 +11,30 @@ public struct Pool: Identifiable {
         public let blockchainName: String
         public let blockchainAddress: String
         public let blockchainDecimals: UInt8
+        
+        public init(id: EthereumAddress, name: String, symbol: String, logo: String, blockchainId: BigUInt, blockchainName: String, blockchainAddress: String, blockchainDecimals: UInt8) {
+            self.id = id
+            self.name = name
+            self.symbol = symbol
+            self.logo = logo
+            self.blockchainId = blockchainId
+            self.blockchainName = blockchainName
+            self.blockchainAddress = blockchainAddress
+            self.blockchainDecimals = blockchainDecimals
+        }
     }
     
     public let id: String
     public let baseAsset: Asset
     public let quoteAsset: Asset
-    public let fee: BigUInt
     public let minOrderSize: BigUInt
     public let maxOrderSize: BigUInt
     
-    init(model: PoolModel, baseAsset: Asset, quoteAsset: Asset) {
-        self.id = model.id.hexString
+    public init(id: String, baseAsset: Asset, quoteAsset: Asset, minOrderSize: BigUInt, maxOrderSize: BigUInt) {
+        self.id = id
         self.baseAsset = baseAsset
         self.quoteAsset = quoteAsset
-        self.fee = model.fee
-        self.minOrderSize = model.minOrderSize
-        self.maxOrderSize = model.maxOrderSize
+        self.minOrderSize = minOrderSize
+        self.maxOrderSize = maxOrderSize
     }
 }
