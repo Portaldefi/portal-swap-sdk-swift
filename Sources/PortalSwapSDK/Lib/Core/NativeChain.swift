eed5abfa000ea436e2191f32fa47c5fc853ec124
cuhte3 <farid@getportal.co>
2025-07-29 20:28:12 +0700

    Update NativeChain.swift

--- Sources/PortalSwapSDK/Lib/Core/NativeChain.swift
+++ Sources/PortalSwapSDK/Lib/Core/NativeChain.swift
@@ -2,7 +2,81 @@ import Foundation
 import BigInt
 import Promises
 
-protocol NativeChain: BaseClass {
+protocol TxLockable: BaseClass {
+    var queue: TransactionLock { get set }
+
+    func waitForReceipt(txid: String) -> Promise<Void>
+    func emitOnFinality(_ txid: String, event: String, args: [Any])
+    func withTxLock<T>(_ asyncFn: @escaping () -> Promise<T>) -> Promise<T>
+    func retryWithBackoff<T>(_ fn: @escaping () -> Promise<T>) -> Promise<T>
+}
+
+extension TxLockable {
+    func emitOnFinality(_ txid: String, event: String, args: [Any]) {
+        waitForReceipt(txid: txid).then { _ in
+            let capitalizedEvent = "on\(event.prefix(1).uppercased())\(event.dropFirst())"
+            self.info(capitalizedEvent, args)
+            self.emit(event: event, args: args)
+        }
+    }
+    
+    func withTxLock<T>(_ asyncFn: @escaping () -> Promise<T>) -> Promise<T> {
+        queue.run(asyncFn)
+    }
+    
+    func retryWithBackoff<T>(_ fn: @escaping () -> Promise<T>) -> Promise<T> {
+        Promise<T> { resolve, reject in
+            let stages = [
+                [1, 0], // 1 attempt immediately
+                [10, 1000], // 10 attempts every 1 second
+                // [10, 2000], // 10 attempts every 2 seconds
+                // [10, 3000], // 10 attempts every 3 seconds
+            ]
+            
+            func tryNextStage(stageIndex: Int) {
+                guard stageIndex < stages.count else {
+                    // All retries exhausted, try one final time to get the actual error
+                    fn().then { result in
+                        resolve(result)
+                    }.catch { error in
+                        reject(error)
+                    }
+                    return
+                }
+                
+                let stage = stages[stageIndex]
+                let attempts = stage[0]
+                let delay = stage[1]
+                
+                func tryAttempt(attemptIndex: Int) {
+                    fn().then { result in
+                        resolve(result)
+                    }.catch { error in
+                        if attemptIndex == attempts - 1 {
+                            // Last attempt of this stage, continue to next stage
+                            tryNextStage(stageIndex: stageIndex + 1)
+                        } else {
+                            // More attempts in this stage
+                            if delay > 0 {
+                                DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(delay)) {
+                                    tryAttempt(attemptIndex: attemptIndex + 1)
+                                }
+                            } else {
+                                tryAttempt(attemptIndex: attemptIndex + 1)
+                            }
+                        }
+                    }
+                }
+                
+                tryAttempt(attemptIndex: 0)
+            }
+            
+            tryNextStage(stageIndex: 0)
+        }
+    }
+}
+
+protocol NativeChain: BaseClass, TxLockable {
     var address: String { get }
 
     func start() -> Promise<Void>
@@ -13,16 +87,6 @@ protocol NativeChain: BaseClass {
     func createInvoice(_ party: Party) -> Promise<Invoice>
     func payInvoice(_ party: Party) -> Promise<Void>
     func settleInvoice(for party: Party, with secret: Data) -> Promise<Party>
-}
-
-extension NativeChain {
-    func emitWithDelay(event: String, args: [Any]) {
-        DispatchQueue.sdk.asyncAfter(deadline: .now() + 3) {
-            let capitalizedEvent = "on\(event.prefix(1).uppercased())\(event.dropFirst())"
-            self.info(capitalizedEvent, args)
-            self.emit(event: event, args: args)
-        }
-    }
 }
 
 final class NativeChainError: BaseError {
